a0fa61a09a974ebd268721d06b6e9639239225d1
shynehua <443557125@qq.com>
2016-04-08 15:12:48 +0800

    schedular and topo

---
+++ scheduler.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+'''
+schedular 
+
+main function :
+ 1. get topo info
+ 2. get flow info from ..
+ 3. get topo status via floodlight rest api
+ 4. routing 
+ 5. provision flow table
+
+author : Xiahe Liu
+
+original version date: 2016/03/05
+'''
+
+import logging
+import json
+import topo
+import SimpleHTTPServer
+import SocketServer
+import sys
+import time
+import urllib2
+from collections import defaultdict
+import threading
+import os
+
+#import SchedularHttpServer
+#from flow import *
+#from routing import *
+#from provision import *
+
+DEBUG = 1
+
+#global portinfo,portbytes,mylock
+portinfo = defaultdict(lambda:defaultdict(lambda:None))
+portbytes = defaultdict(lambda:defaultdict(lambda:None))
+topology = topo.topo()
+requestflow = []
+#mylock = threading.Lock() 
+
+# multi-threading class
+class MyThread(threading.Thread):
+    def __init__(self, func, args, name=''):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.func = func
+        self.args = args
+    
+    def run(self):
+        self.res = apply(self.func, self.args)
+    
+    def getResult(self):
+        return self.res
+        
+
+def simple_json_get(url):
+    return json.loads(urllib2.urlopen(url).read())
+
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        logging.warning("======= GET STARTED =======")
+        logging.warning(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        logging.warning("======= POST STARTED =======")
+        logging.warning(self.headers)
+        length = self.headers.getheader('content-length');
+        nbytes = int(length)
+        rawdata = self.rfile.read(nbytes)
+        logging.warning(rawdata)
+        logging.warning(type(rawdata))
+        data = json.loads(rawdata)
+        logging.warning("======= POST DATA =======")
+        
+        # process the request 
+        coflow_process(data)
+
+        # Begin the response
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write('Client: %s\n' % str(self.client_address))
+        self.wfile.write('User-agent: %s\n' % str(self.headers['user-agent']))
+        self.wfile.write('Path: %s\n' % self.path)
+        self.wfile.write(rawdata)
+
+
+    
+    # get reveiceved and transmitted bytes of each port in each switch
+def getPortBytes():
+    global portbytes,mylock
+    host = '192.168.109.214'
+    port = 8080
+    if len(sys.argv) >=2:
+        host = sys.argv[1]
+    if len(sys.argv) >=3:
+        port = sys.argv[2]
+        
+    URL = 'http://%s:%d/wm/core/switch/all/port/json' % (host,port)
+    portstatus = simple_json_get(URL)
+    #print portstatus
+    
+    for dpid in portstatus:
+        for j in portstatus[dpid]['port_reply'][0]['port']:
+            portnum = j['portNumber']
+            rbytes = long(j['receiveBytes'])
+            tbytes = long(j['transmitBytes'])
+            if portbytes[dpid][portnum] == None:
+                portbytes[dpid][portnum] = [rbytes,tbytes]
+            else:
+                portbytes[dpid][portnum][0] = rbytes
+                portbytes[dpid][portnum][1] = tbytes
+    
+        
+# calculate average RX and TX during the past interval seconds
+def getPortRate(interval):
+    global portinfo,portbytes,mylock
+    
+    while True:
+        getPortBytes()
+        
+        #mylock.acquire()
+        for dpid in portbytes:
+            for portnum in portbytes[dpid]:
+                newrbyte = portbytes[dpid][portnum][0]
+                newtbyte = portbytes[dpid][portnum][1]
+                #print type(newrbyte), newtbyte
+                if portinfo[dpid][portnum] == None:
+                    portinfo[dpid][portnum] = [newrbyte/interval,newtbyte/interval,newrbyte,newtbyte]
+                else:
+                    oldrbyte = portinfo[dpid][portnum][2]
+                    oldtbyte = portinfo[dpid][portnum][3]
+                    portinfo[dpid][portnum] = [(newrbyte-oldrbyte)/interval,(newtbyte-oldtbyte)/interval,newrbyte,newtbyte]
+        
+        #print time.time()
+        #print portinfo
+        time.sleep(interval)
+def flow_table_pusher(flowtables):
+    
+    host = '192.168.109.214'
+    port = 8080
+    if len(sys.argv) >=2:
+        host = sys.argv[1]
+    if len(sys.argv) >=3:
+        port = sys.argv[2]
+        
+    url = 'http://%s:%d/wm/staticflowpusher/json' % (host,port)
+    
+    for flow in flowtables:
+        jdata = json.dumps(flow)
+        req = urllib2.Request(url, jdata)
+        response = urllib2.urlopen(req)
+        print response.read()
+        
+def flow_table_delete(flowtables):
+    
+    host = '192.168.109.214'
+    port = 8080
+    if len(sys.argv) >=2:
+        host = sys.argv[1]
+    if len(sys.argv) >=3:
+        port = sys.argv[2]
+        
+    url = 'http://%s:%d/wm/staticflowpusher/json' % (host,port)
+    
+    for flow in flowtables:
+        jdata = json.dumps(flow)
+        req = urllib2.Request(url, data=jdata)
+        req.get_method = lambda: 'DELETE'
+        response = urllib2.urlopen(req)
+        print response.read()
+        
+def flow_table_info(topology, path, pro):
+    pathnumstr = ""
+    for i in path:
+        pathnumstr += str(i)
+    #print pathnumstr
+    
+    flowtables = []
+
+    for i in range(len(path)-1):
+        if i == 0:
+            continue
+        preNode = path[i-1]
+        curNode = path[i]
+        nextNode = path[i+1]
+        #print preNode, curNode, nextNode
+        
+        dpid = topology.node[curNode][0]
+        inport = topology.adj[preNode][curNode][2]
+        outport = topology.adj[curNode][nextNode][1]
+        #print dpid, inport, outport
+        # set metrics in a flow table
+        flow = {}
+        flow["switch"] = "%s" % dpid
+        flow["name"] = "flow-mod-%s%d" % (pathnumstr,curNode)
+        flow["cookie"] = "0"
+        flow["priority"] = "32768"
+        flow["ingress-port"] = "%d" % inport
+        flow["active"] = "true"
+        flow["actions"] = "output=%d" % outport
+        
+        #print flow
+        flowtables.append(flow)
+    if pro: 
+        flow_table_pusher(flowtables)
+    return flowtables
+    
+'''
+coflow_process
+function:
+    handle coflow requests
+'''
+def coflow_process(request):
+    global portinfo,topology
+
+    if DEBUG == 1:
+        logging.warning('=======port information=======')
+        logging.warning(portinfo)
+        
+    # get topo status and update topo info  
+    topology.update_adj_band(portinfo)
+        
+    # get the topology information for routing  
+    routingTopo = topology.get_routing_topo()
+    if DEBUG == 1:
+        logging.warning('=======routing topo =======')
+        logging.warning(routingTopo)
+    
+    # routing
+    #path = shortest_path()
+    path = [5,3,1,4,7]
+    
+    # provision flow table
+    flows = flow_table_info(topology, path, False)
+    logging.warning(flows)
+    logging.warning('=======flow pusher end =======')
+    requestflow.append(request)
+    requestflow.append(flows)
+    logging.warning('=======Request preocess end =======')
+    
+    logging.warning(requestflow)
+
+def main():
+    # port RX/TX calculating
+    interval = 5
+    
+    t2 = MyThread(getPortRate, [interval], "RX/TX calculating")
+    t2.isDaemon()
+    t2.setDaemon(True)
+    t2.start()
+    
+    if DEBUG == 1:
+        time.sleep(5)
+        logging.warning('main')
+        logging.warning(portinfo)
+    
+    
+    Handler = ServerHandler
+    httpd = SocketServer.TCPServer(("localhost", 8000), Handler)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
---
+++ topo.py
@@ -0,0 +1,201 @@
+from collections import defaultdict
+#import urllib2
+import json
+import logging
+
+class topo(object):
+    def __init__(self):
+        # hosts & switches
+        self.swicthNum = 4
+        self.hostNum = 4
+        self.node = defaultdict(lambda:None)
+        # switch[i] = [dpid, mac, ipv4, port]
+        # the node whose id <= swicthNum is switch node
+        self.node[1] = ['00:00:00:1b:cd:03:19:90', '00:1b:cd:03:19:90', '192.168.109.224', '45915']
+        self.node[2] = ['00:00:00:1b:cd:03:16:ac', '00:1b:cd:03:16:ac', '192.168.109.225', '56741']
+        self.node[3] = ['00:00:00:1b:cd:03:04:64', '00:1b:cd:03:04:64', '192.168.109.214', '42058']
+        self.node[4] = ['00:00:00:1b:cd:03:05:94', '00:1b:cd:03:05:94', '192.168.109.215', '53878']
+
+        # host[i] = [mac, ipv4, ipv6, attachedswicth, switchport, switchId]
+        # the node whose id > swicthNum and id <= swicthNum + hostNUm is host node
+        self.node[5] = ['f8:0f:41:f6:68:4e', '10.0.0.212', 'fe80::fa0f:41ff:fef6:684e', '00:00:00:1b:cd:03:04:64',1,3]
+        self.node[6] = ['f8:0f:41:f6:68:4b', '10.0.0.213', 'fe80::fa0f:41ff:fef6:684b', '00:00:00:1b:cd:03:04:64',2,3]
+        self.node[7] = ['f8:0f:41:f6:63:41', '10.0.0.211', 'fe80::fa0f:41ff:fef6:6341', '00:00:00:1b:cd:03:05:94',1,4]
+        self.node[8] = ['f8:0f:41:f4:2a:1b', '10.0.0.201', 'fe80::72e2:84ff:fe05:6767', '00:00:00:1b:cd:03:05:94',2,4]
+
+        
+        
+        # topology
+        # adjacency[1][2] = [link_id, outport, inport, bandwidth]
+        self.adj = defaultdict(lambda:defaultdict(lambda:None))
+        self.defaultband = 1000
+        self.adj[1][3] = [1, 1, 3, self.defaultband]
+        self.adj[3][1] = [2, 3, 1, self.defaultband]
+        self.adj[1][4] = [3, 2, 3, self.defaultband]
+        self.adj[4][1] = [4, 3, 2, self.defaultband]
+        self.adj[2][3] = [5, 4, 4, self.defaultband]
+        self.adj[3][2] = [6, 4, 4, self.defaultband]
+        self.adj[2][4] = [7, 5, 4, self.defaultband]
+        self.adj[4][2] = [8, 4, 5, self.defaultband]
+        
+        self.adj[3][5] = [9, 1, 0, self.defaultband]
+        self.adj[5][3] = [10, 0, 1,self.defaultband]
+        self.adj[3][6] = [11, 2, 0,self.defaultband]
+        self.adj[6][3] = [12, 0, 2,self.defaultband]
+        self.adj[4][7] = [13, 1, 0,self.defaultband]
+        self.adj[7][4] = [14, 0, 1, self.defaultband]
+        self.adj[4][8] = [15, 2, 0, self.defaultband]
+        self.adj[8][4] = [16, 0, 2, self.defaultband]
+        
+
+        self.topo = defaultdict(lambda:defaultdict(lambda:None))
+        #topo[dpid][portnum] = [rate,src,dst]
+        self.topo['00:00:00:1b:cd:03:19:90'][1] = [1, 3] #TX
+        self.topo['00:00:00:1b:cd:03:19:90'][2] = [1, 4] #TX
+        self.topo['00:00:00:1b:cd:03:16:ac'][4] = [2, 3] #TX
+        self.topo['00:00:00:1b:cd:03:16:ac'][5] = [2, 4] #TX
+        
+        self.topo['00:00:00:1b:cd:03:04:64'][3] = [3, 1] #TX
+        self.topo['00:00:00:1b:cd:03:04:64'][4] = [3, 2] #TX
+        self.topo['00:00:00:1b:cd:03:05:94'][3] = [4, 1] #TX
+        self.topo['00:00:00:1b:cd:03:05:94'][4] = [4, 2] #TX
+        
+        self.topo['00:00:00:1b:cd:03:04:64'][1] = [3, 5] #TX
+        self.topo['00:00:00:1b:cd:03:04:64'][2] = [3, 6] #TX
+        self.topo['00:00:00:1b:cd:03:04:64'][11] = [5, 3] #RX
+        self.topo['00:00:00:1b:cd:03:04:64'][12] = [6, 3] #RX
+        
+        self.topo['00:00:00:1b:cd:03:05:94'][1] = [4, 7] #TX
+        self.topo['00:00:00:1b:cd:03:05:94'][2] = [4, 8] #TX
+        self.topo['00:00:00:1b:cd:03:05:94'][11] = [7, 4] #RX
+        self.topo['00:00:00:1b:cd:03:05:94'][12] = [8, 4] #RX
+        #self.routingTopo = self.get_routing_topo()
+        
+    def update_adj_band(self,portinfo):
+        bytes2mb = 1048576
+        for dpid in portinfo:
+            for portnum in portinfo[dpid]:
+                if portnum == 'local':
+                    continue
+                portnumint = int(portnum)
+                if self.topo[dpid][portnumint] == None:
+                    continue
+                    
+                src = self.topo[dpid][portnumint][0]
+                dst = self.topo[dpid][portnumint][1]
+                self.adj[src][dst][3] = self.defaultband - portinfo[dpid][portnum][1]/bytes2mb
+
+                if (dpid=='00:00:00:1b:cd:03:04:64' or dpid == '00:00:00:1b:cd:03:05:94') and (portnumint ==1 or portnumint ==2):
+                    portnumint = portnumint + 10
+                    src2 = self.topo[dpid][portnumint][0]
+                    dst2 = self.topo[dpid][portnumint][1]
+                    self.adj[src2][dst2][3] = self.defaultband - portinfo[dpid][portnum][0]/bytes2mb
+        print self.adj
+        return True
+        
+    def get_routing_topo(self):
+        # topology with weight infomation
+        print "test1"
+        routingTopo = defaultdict(lambda:defaultdict(lambda:None))
+        for i in self.adj:
+            for j in self.adj[i]:
+                tmp = self.adj[i][j][3]
+                if tmp!=0:
+                    routingTopo[i][j] = tmp
+        #print routingTopo
+        return routingTopo
+    
+    def update_adj(self,src,dst,info,newlink):
+        # add a new link or update exist link
+        if newlink:
+            self.adj[src][dst] = info
+        elif self.adj[src][dst] != None:
+            self.adj[src][dst] = info
+        else:
+            raise KeyError
+
+    def update_node_info(self,nodeId,info,newnode):
+        # add a new node or update exist node
+        if newnode:
+            self.node[nodeId] = info
+        elif self.node[nodeId] != None:
+            self.node[nodeId] = info
+        else:
+            raise KeyError
+            
+    
+    '''       
+    # discard
+    # originally wanna to use this get bandwidth from floodlight(bug here,cannot get port information)
+    # then update topology information
+    def simple_json_get(self, url):
+        return json.loads(urllib2.urlopen(url).read())
+        
+    def get_topo_status(self):
+        host = "192.168.109.214"
+        port = 8080
+        
+        for i in self.adj:
+            for j in self.adj[i]:
+                if j <= i:
+                    continue
+                swicthId = self.node[i][0]
+                portId = self.adj[i][j][1]
+                print i,j, swicthId, portId
+                URL = "http://%s:%d/wm/statistics/bandwidth/%s/%d/json" % (host,port,swicthId,portId)
+                ### need to process the FL bandwidth output !!!!!!!!!!!!!!!!!!!!!
+                new = self.simple_json_get(URL)
+                
+                {
+                "bits-per-second-rx": "0",
+                "bits-per-second-tx": "0",
+                "dpid": "00:00:00:00:00:00:00:01",
+                "port": "local",
+                "updated": "Wed Feb 17 10:42:20 EST 2016"
+                }
+                
+                print new
+                old = self.adj[i][j][3]
+                if old != new:
+                    info = self.adj[i][j]
+                    info[3] = new
+                    self.update_topo(i,j,info,False)
+    '''
+    
+def test():
+    topology = topo()
+    '''
+    print topology.node
+    print topology.adj
+    '''
+    
+    '''
+    info = topology.node[1]
+    info[0] = '0000'
+    topology.update_node_info(1,info,True)
+    print topology.node[1]
+    '''
+    
+    '''
+    tmp = topology.get_routing_topo()
+    for i in tmp:
+        for j in tmp[i]:
+            print i, j, tmp[i][j]
+    '''
+    
+    #topology.get_topo_status()
+    '''
+    try:
+        f = open('tmp.txt','r')
+    except IOError:
+        logging.warning('cannot open file')
+    rawdata = f.read()
+    data = json.loads(rawdata)
+    #print data
+    f.close()
+
+    topology.update_adj_band(data)
+    '''
+    
+if __name__ == '__main__':
+    test()
