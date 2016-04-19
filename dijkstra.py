2988e7c15fdb39e53d4a6aaed945700760910cd8
mons <msystudio2@gmail.com>
2016-04-19 14:20:36 +0800

    Could use numpy and networkx to perform easy Dijsktra (ref: SO)

---
+++ dijkstra.py
@@ -0,0 +1,10 @@
+import networkx as nx
+import numpy as np
+
+A = np.array([[0, 4, 3, 0],
+              [0, 0, 0, 1],
+              [0, 3, 0, 1],
+              [2, 0, 0, 0]])
+
+G = nx.from_numpy_matrix(A, create_using=nx.DiGraph())
+print(nx.dijkstra_path(G, 0, 3))
