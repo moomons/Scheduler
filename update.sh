6c57e15690e5679961aa8fa5fb49d3db7f2109bc
mons <msystudio2@gmail.com>
2016-04-19 16:39:55 +0800

    Update script: Fix cd path

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 echo 'Version 0.10'
 echo -e 'Script function: Backup and replace some Hadoop modules.\n Author: mons \n Date: 2016/04/10\n'
 
-cd '~/hadoop/hadoop-2.7.1/share/hadoop/mapreduce/'
+cd ~/hadoop/hadoop-2.7.1/share/hadoop/mapreduce/
 
 if [ ! -f 'hadoop-mapreduce-client-core-2.7.1.jar.ORIGINAL' ]; then
     echo "Backup file NOT FOUND. Renamed original file to .jar.ORIGINAL"
