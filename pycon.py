e4ae44fc18c46cbb4b6a2fbc733680ef9feb7f3c
mons <msystudio2@gmail.com>
2016-04-24 13:26:29 +0800

    Switches and hosts links done

--- pycon.py
+++ pycon.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+"""
+PyCon
+
+Code completely rewritten by mons in a modern and efficient manner
+"""
+
 from collections import defaultdict
 from pandas import *
 import json
@@ -17,27 +23,39 @@ floodlight_port = 8080
 # TODO: Create the topo from FL REST API return info
 topo = defaultdict(lambda: defaultdict(lambda: None))
 
-# TODO: Using HTTP POST to get the return value from FL Controller
-URL_REST_API_switch_links = 'http://%s:%d/wm/topology/links/json' % (floodlight_host, floodlight_port)
-URL_REST_API_host2switch_links = 'http://%s:%d/wm/device/' % (floodlight_host, floodlight_port)
-API_Result = pycon_def.json_get_from_url(URL_REST_API_switch_links)
-
-# print json.dumps(API_Result, sort_keys=True, indent=2, separators=(',', ': '))
-
 # Extract switches and nodes links info
 Mat_Links = defaultdict(lambda: defaultdict(lambda: None))
+
+URL_REST_API_switch_links = 'http://%s:%d/wm/topology/links/json' % (floodlight_host, floodlight_port)
+URL_REST_API_host2switch_links = 'http://%s:%d/wm/device/' % (floodlight_host, floodlight_port)
+
+API_Result = pycon_def.json_get_from_url(URL_REST_API_switch_links)
+# curl 127.0.0.1:8080/wm/topology/links/json|pjt
+# print json.dumps(API_Result, sort_keys=True, indent=2, separators=(',', ': '))
 try:
     for e in API_Result:
-        pprint.pprint(e)
-        Mat_Links[e['src-switch']][e['dst-switch']] = [e['src-port'], e['dst-port']]
-        Mat_Links[e['dst-switch']][e['src-switch']] = [e['dst-port'], e['src-port']]
-
-        # print(e['src-switch'])
-        # print(e['src-port'])
+        # pprint.pprint(e)
+        Mat_Links[e['src-switch'][-5:]][e['dst-switch'][-5:]] = [e['src-port'], e['dst-port']]
+        Mat_Links[e['dst-switch'][-5:]][e['src-switch'][-5:]] = [e['dst-port'], e['src-port']]
+        # MARK: REMOVE the [-5:] before running this script in production environments
 except KeyError:
-    print 'KeyError: The FL return value may have changed or the URL is incorrect.'
+    print 'KeyError: Are you sure the FL is up?'
 
-print Mat_Links
+API_Result = pycon_def.json_get_from_url(URL_REST_API_host2switch_links)
+# curl 127.0.0.1:8080/wm/device/|pjt
+if len(API_Result) == 0:
+    print 'Error: No hosts detected. Please init the hosts before running this script. For example: pingall'
+    exit(-1)
+# print json.dumps(API_Result, sort_keys=True, indent=2, separators=(',', ': '))
+try:
+    for e in API_Result:
+        # pprint.pprint(e)
+        d = e['attachmentPoint'][0]  # Extract the dict inside the list
+        Mat_Links[d['switchDPID'][-5:]][e['ipv4'][0]] = [d['port'], 0]
+        Mat_Links[e['ipv4'][0]][d['switchDPID'][-5:]] = [0, d['port']]
+        # MARK: REMOVE the [-5:] before running this script in production environments
+except KeyError:
+    print 'KeyError: Are you sure the FL is up?'
 
 fd = DataFrame(Mat_Links).T.fillna(0)
 print fd
