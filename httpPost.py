6c3ddffa257f63ec280e88177a3054da57daf716
mons <msystudio2@gmail.com>
2016-04-25 18:24:59 +0800

    Test http server module done

--- httpPost.py
+++ httpPost.py
@@ -1,18 +1,41 @@
-# http post
+# http post tester
 
 import urllib2
 import json
+import logging
 
-    
+url = 'http://127.0.0.1:7999'
+
+
 def http_post():
-    url='http://192.168.32.111:7999'
-    values ={'coflowId':1,'flowId':1, 'sour':'10.0.0.1', 'dst':'10.0.0.2', 'size':100}
-    jdata = json.dumps(values)
-    req = urllib2.Request(url, jdata)
-    response = urllib2.urlopen(req)
-    print response.read()
+    for i in range(1, 1):
+        logging.warning("Sending request: MR-" + str(i))
+        values_MR = {
+            'coflowId': 'attempt_1461250880434_0003_m_000017_0',
+            'src': 'localhost:13562',  # Test if socket can DNS query this and get 127.0.0.1 first
+            'len': 9285750,  # in bytes
+            'flowId': 998,
+            'dst': 'Sender',
+        }
+        jdata = json.dumps(values_MR)
+        req = urllib2.Request(url, jdata)
+        response = urllib2.urlopen(req)
+        print response.read()
+
+        logging.warning("Sending request: FL-" + str(i))
+        values_FL = {
+            'para_map': 'attempt_1461250880434_0003_m_000017_0',  # Test multiple attempts later sep with ','
+            'para_job': 'job_1461250880434_0003',  # the 'Real' coflowId
+            'srcPort': 40108,  # the most important info here
+            'para_reduce': 0,
+        }
+        jdata = json.dumps(values_FL)
+        req = urllib2.Request(url, jdata)
+        response = urllib2.urlopen(req)
+        print response.read()
 
 
 if __name__ == '__main__': 
     http_post()
+    logging.warning("httpPost done.")
 
