bdd4f58d2fbb510059c3f3a86971c75185cd1e90
mons <msystudio2@gmail.com>
2016-04-25 18:28:57 +0800

    Improved test POST module

--- httpPost.py
+++ httpPost.py
@@ -8,31 +8,34 @@ url = 'http://127.0.0.1:7999'
 
 
 def http_post():
-    for i in range(1, 1):
-        logging.warning("Sending request: MR-" + str(i))
-        values_MR = {
-            'coflowId': 'attempt_1461250880434_0003_m_000017_0',
-            'src': 'localhost:13562',  # Test if socket can DNS query this and get 127.0.0.1 first
-            'len': 9285750,  # in bytes
-            'flowId': 998,
-            'dst': 'Sender',
-        }
-        jdata = json.dumps(values_MR)
-        req = urllib2.Request(url, jdata)
-        response = urllib2.urlopen(req)
-        print response.read()
+    try:
+        for i in range(1, 2):
+            logging.warning("Sending request: MR-" + str(i))
+            values_MR = {
+                'coflowId': 'attempt_1461250880434_0003_m_000017_0',
+                'src': 'localhost:13562',  # Test if socket can DNS query this and get 127.0.0.1 first
+                'len': 9285750,  # in bytes
+                'flowId': 998,
+                'dst': 'Sender',
+            }
+            jdata = json.dumps(values_MR)
+            req = urllib2.Request(url, jdata)
+            response = urllib2.urlopen(req)
+            print response.read()
 
-        logging.warning("Sending request: FL-" + str(i))
-        values_FL = {
-            'para_map': 'attempt_1461250880434_0003_m_000017_0',  # Test multiple attempts later sep with ','
-            'para_job': 'job_1461250880434_0003',  # the 'Real' coflowId
-            'srcPort': 40108,  # the most important info here
-            'para_reduce': 0,
-        }
-        jdata = json.dumps(values_FL)
-        req = urllib2.Request(url, jdata)
-        response = urllib2.urlopen(req)
-        print response.read()
+            logging.warning("Sending request: FL-" + str(i))
+            values_FL = {
+                'para_map': 'attempt_1461250880434_0003_m_000017_0',  # Test multiple attempts later sep with ','
+                'para_job': 'job_1461250880434_0003',  # the 'Real' coflowId
+                'srcPort': 40108,  # the most important info here
+                'para_reduce': 0,
+            }
+            jdata = json.dumps(values_FL)
+            req = urllib2.Request(url, jdata)
+            response = urllib2.urlopen(req)
+            print response.read()
+    except urllib2.URLError:
+        logging.error("Check your connection.")
 
 
 if __name__ == '__main__': 
