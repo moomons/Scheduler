d978aeadb15b904c505166e493e55f865fe4ce80
mons <msystudio2@gmail.com>
2016-04-29 23:30:53 +0800

    Better logging (to both screen and file!)

--- pycon_cfg.py
+++ pycon_cfg.py
@@ -17,29 +17,53 @@ class SchedulingAlgo(Enum):
 
 
 # LOGGING
+# import logging
+# logger = logging.getLogger('PyCon')  # create logger
+# logger.setLevel(logging.DEBUG)
+# ch = logging.StreamHandler()  # create console handler and set level to debug
+# ch.setLevel(logging.DEBUG)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # create formatter
+# ch.setFormatter(formatter)  # add formatter to ch
+# logger.addHandler(ch)  # add ch to logger
+#
+#
+# import logging.handlers
+# LOG_FILE = 'Logfile' + '.log'
+#
+# handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024*1024, backupCount=998)
+# # fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+# fmt = '%(asctime)s - %(levelname)s - %(message)s'
+#
+# formatter = logging.Formatter(fmt)   # instantiate formatter
+# handler.setFormatter(formatter)      # add formatter to handler
+#
+# fileLogger = logging.getLogger('PyCon')  # get logger
+# fileLogger.addHandler(handler)           # add handler to fileLogger
+# fileLogger.setLevel(logging.DEBUG)
+#
+# fileLogger.info('fileLogger started')
+# # fileLogger.debug('first debug message')
+
 import logging
-logger = logging.getLogger('PyCon')  # create logger
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()  # create console handler and set level to debug
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # create formatter
-ch.setFormatter(formatter)  # add formatter to ch
-logger.addHandler(ch)  # add ch to logger
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M:%S',
+                    filename='pyconLog.log',
+                    filemode='w')
+# WARNING: Will overwrite the last log file!
 
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
 
-import logging.handlers
-LOG_FILE = 'Logfile' + '.log'
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
 
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024*1024, backupCount=998)
-# fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
-fmt = '%(asctime)s - %(levelname)s - %(message)s'
+logging.getLogger('').addHandler(console)
 
-formatter = logging.Formatter(fmt)   # instantiate formatter
-handler.setFormatter(formatter)      # add formatter to handler
+logging.info('Application started.')
 
-fileLogger = logging.getLogger('PyCon')  # get logger
-fileLogger.addHandler(handler)           # add handler to fileLogger
-fileLogger.setLevel(logging.DEBUG)
+logger = logging.getLogger('PyCon Main')
+fileLogger = logging.getLogger('BW Matrix')
 
-fileLogger.info('fileLogger started')
-# fileLogger.debug('first debug message')
+# Ref: http://blog.csdn.net/ghostfromheaven/article/details/8249298
+
