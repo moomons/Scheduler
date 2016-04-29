6bc017a1f69536e3b2ee30baf501c6c1b92e4c29
mons <msystudio2@gmail.com>
2016-04-30 00:04:16 +0800

    Clean code

--- pycon_cfg.py
+++ pycon_cfg.py
@@ -1,4 +1,10 @@
 
+"""
+
+Stores some configurations and loggers for PyCon
+
+"""
+
 from enum import Enum
 
 # Configurations and global variables
@@ -14,35 +20,7 @@ class SchedulingAlgo(Enum):
     More = 3
 
 
-
-
 # LOGGING
-# import logging
-# logger = logging.getLogger('PyCon')  # create logger
-# logger.setLevel(logging.DEBUG)
-# ch = logging.StreamHandler()  # create console handler and set level to debug
-# ch.setLevel(logging.DEBUG)
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # create formatter
-# ch.setFormatter(formatter)  # add formatter to ch
-# logger.addHandler(ch)  # add ch to logger
-#
-#
-# import logging.handlers
-# LOG_FILE = 'Logfile' + '.log'
-#
-# handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024*1024, backupCount=998)
-# # fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
-# fmt = '%(asctime)s - %(levelname)s - %(message)s'
-#
-# formatter = logging.Formatter(fmt)   # instantiate formatter
-# handler.setFormatter(formatter)      # add formatter to handler
-#
-# fileLogger = logging.getLogger('PyCon')  # get logger
-# fileLogger.addHandler(handler)           # add handler to fileLogger
-# fileLogger.setLevel(logging.DEBUG)
-#
-# fileLogger.info('fileLogger started')
-# # fileLogger.debug('first debug message')
 
 import logging
 logging.basicConfig(level=logging.DEBUG,
@@ -50,6 +28,8 @@ logging.basicConfig(level=logging.DEBUG,
                     datefmt='%m-%d %H:%M:%S',
                     filename='pyconLog.log',
                     filemode='w')
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # create formatter
+# fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
 # WARNING: Will overwrite the last log file!
 
 console = logging.StreamHandler()
