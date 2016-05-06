503e6e9a74b4058eabab7fd4bf21c4e875dc66cf
mons <msystudio2@gmail.com>
2016-05-06 21:35:58 +0800

    Now logging in msecs

--- pycon_cfg.py
+++ pycon_cfg.py
@@ -26,7 +26,7 @@ CurrentSchedulingAlgo = SchedulingAlgo.SEBF
 
 import logging
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M:%S',
                     filename='pyconLog.log',
                     filemode='w')
