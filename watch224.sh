afde567afcf531071103320677052e5fa29f8e86
mons <msystudio2@gmail.com>
2016-05-11 09:53:27 +0800

    The bash script can iterate through all the switches now.

--- watch224.sh
+++ watch224.sh
@@ -5,37 +5,42 @@ bold="\033[1m"
 normal="\033[0m"
 cyan="\033[36m"
 underline="\033[4m"
+threshold=60000
+ovsip=(192.168.109.214 192.168.109.215 192.168.109.224 192.168.109.225)
+LOG_FILE="watch.log"
+exec 3>&1 1>>${LOG_FILE} 2>&1
+# log ref: http://stackoverflow.com/a/18462920/5676271
 
-while :
-do
+while; do
+    echo -e $bold$greentext"Time: "$(date +"%H:%M:%S")$normal | tee /dev/fd/3
+    for ip in "${ovsip[@]}"; do
+        echo -e $bold$underline"On switch $ip:"#normal | tee /dev/fd/3
+        input_all=$(ovs-ofctl -O OpenFlow13 dump-flows tcp:$ip:6666)
+        input=$(echo $input_all | grep priority=1)  # Only P1
+        echo -e "$input_all" | grep n_packets= | tee /dev/fd/3
 
-echo -e $bold$greentext"Time: "$(date +"%H:%M:%S")$normal
-input=$(sudo ovs-ofctl -O OpenFlow13 dump-flows datanet224 | grep priority=1)
-echo -e "$input" | grep n_packets=
+        counter=0
+        while read -r line
+        do
+            #echo "Current line: $line"
+            #n_pkts=$(awk -v curline="$line" -F '[=,]' '{print $8}')
+            n_pkts=$(echo $line | grep -Po '(?<=(n_packets=))\d*')
+            # if n_pkts is empty, continue
+            #echo "Got n_packets = $n_pkts"
+            if (( "$n_pkts" >= "$threshold" )); then
+                echo "Eliminating n_pkts = $n_pkts:" | tee /dev/fd/3
+                cmd="sudo ovs-ofctl -O OpenFlow13 del-flows tcp:$ip:6666 "$(echo $line | grep -Po 'tcp\S*')
+                echo "Command: $cmd" | tee /dev/fd/3
+                eval $cmd
+                ((counter++))
+            fi
+        done <<< "$input"
 
-counter=0
-while read -r line
-do
-    #echo "Current line: $line"
-    #n_pkts=$(awk -v curline="$line" -F '[=,]' '{print $8}')
-    n_pkts=$(echo $line | grep -Po '(?<=(n_packets=))\d*')
-    #echo "Got n_packets = $n_pkts"
-    if (( "$n_pkts" >= 50000 )); then
-        echo "Eliminating n_pkts = $n_pkts:"
-        cmd="sudo ovs-ofctl -O OpenFlow13 del-flows datanet224 "$(echo $line | grep -Po 'tcp\S*')
-        echo "Command: $cmd"
-        eval $cmd
-        ((counter++))
-    fi
-done <<< "$input"
+        if (( counter > 0 )); then
+            echo -e $cyan$underline"Flow entry eliminated: $counter at $ip"$normal | tee /dev/fd/3
+        fi
 
-if (( counter > 0 )); then
-    echo -e $cyan$underline"Flow entry eliminated: $counter"$normal
-fi
-
-#input=$(sudo ovs-ofctl -O OpenFlow13 dump-flows datanet224 | grep priority=1)
-#echo -e "\n$input"
-
-sleep 2
+    done
+    sleep 2
 done
 
