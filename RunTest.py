60c55cfe2cc0809cc199cf9840fbb13186b5bba7
Scott Mon <Scott Mon>
2016-05-19 20:27:29 +0800

    正在计算延迟

--- RunTest.py
+++ RunTest.py
@@ -21,7 +21,8 @@ from pycon_cfg import *
 import os
 from pprint import pprint
 from enum import Enum
-
+from pycon_def import GetPathList
+from pycon_def import Mat_BW_Cap
 
 
 class FlowType(Enum):
@@ -65,19 +66,19 @@ Flow_To_Generate_Per_SRCDSTPair = [
     [1, [FlowType.HighBandwidth, 0.1, 500, 300, 0]],
 ]
 
+# DictOfFlows_LowLatency = [{'weight': '1.0', 'srcip': '10.0.0.201', 'dstip': '10.0.0.211', 'bandwidth': 8, 'delay': 1000}]
+# DictOfFlows_HighBandwidth = [{'weight': '1.0', 'srcip': '10.0.0.201', 'dstip': '10.0.0.211', 'bandwidth': 500, 'minbandwidth': 250}]
+ListOfFlows_LowLatency = []
+ListOfFlows_HighBandwidth = []
 
-def RunStatic():
-    # TODO: Run ITGLog on 213. Run ITGRecv, ITGSend -Q -L 192.168.109.213 on all
+# Mat_BW_Cap
+Mat_BW_Cap_Remain = Mat_BW_Cap
+Mat_Link_Share = Mat_BW_Cap
 
-    # TODO: Gen config list for ITGController from List_SRC_DST and Flow_To_Generate_Per_SRCDSTPair
-    # MARK: Debugging, just use first pair in List_SRC_DST
 
-    # DictOfFlows_LowLatency = [{'weight': '1.0', 'srcip': '10.0.0.201', 'dstip': '10.0.0.211', 'bandwidth': 8, 'delay': 1000}]
-    # DictOfFlows_HighBandwidth = [{'weight': '1.0', 'srcip': '10.0.0.201', 'dstip': '10.0.0.211', 'bandwidth': 500, 'minbandwidth': 250}]
-    ListOfFlows_LowLatency = []
-    ListOfFlows_HighBandwidth = []
+def GenerateAndSortListOfFlows():
+    global ListOfFlows_LowLatency, ListOfFlows_HighBandwidth
 
-    # Generate ListOfFlows
     for OnePair in List_SRC_DST_Group:
         source = OnePair[0]
         dests = OnePair[1]
@@ -129,12 +130,50 @@ def RunStatic():
     ListOfFlows_LowLatency = sorted(ListOfFlows_LowLatency, key=lambda k: k['weight'], reversed=True)
     ListOfFlows_HighBandwidth = sorted(ListOfFlows_HighBandwidth, key=lambda k: k['weight'], reversed=True)
 
+def GetRemainingBandwidthAndDelay(path, calculate_delay=False):
+    assert(len(path) >= 3)
+    bottleneckbandwidth = Mat_BW_Cap_Remain[path[0]][path[1]]
+    for i in range(1, len(path)-1):
+        rembandwidthonthispath = Mat_BW_Cap_Remain[path[i]][path[i+1]]
+        if rembandwidthonthispath < bottleneckbandwidth:
+            bottleneckbandwidth = rembandwidthonthispath
+
+    delay = 0
+    if calculate_delay:
+        delay = 1
+
+    return bottleneckbandwidth, delay
+
+def RunStatic():
+    # TODO: Run ITGLog on 213. Run ITGRecv, ITGSend -Q -L 192.168.109.213 on all
+
+    # TODO: Gen config list for ITGController from List_SRC_DST and Flow_To_Generate_Per_SRCDSTPair
+    # MARK: Debugging, just use first pair in List_SRC_DST
+
+    # Generate ListOfFlows
+    global ListOfFlows_LowLatency, ListOfFlows_HighBandwidth
+    GenerateAndSortListOfFlows()
 
     # The static algorithm
     logger.info('Static Algorithm starting')
+    # Statistics info to
+    Stat_Accepted = 0
+    Stat_Rejected = 0
+
+    List_AcceptedLL = []
+    List_AcceptedHB = []
+
     for F_LL in ListOfFlows_LowLatency:
         logger.info(F_LL)
+        paths, paths_number = GetPathList(F_LL['srcip'], F_LL['dstip'])
+        for viable_path in paths:
+            bw_rem, delay = GetRemainingBandwidthAndDelay(viable_path, True)  # Note: delay in us, bw_rem in Mbps
+            if F_LL['bandwidth'] <= bw_rem and delay <= F_LL['delay'] and CheckUpdatedDelay(F_LL):
+                # Basic req okay. Check other LL flow delay req.
+                AddFlow_LL(F_LL)
 
+    for F_HB in ListOfFlows_HighBandwidth:
+        logger.info(F_HB)
 
     # TODO: Gen ITGController config-file
 
