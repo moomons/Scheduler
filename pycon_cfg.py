eac333fa86f9834a1142a2d6c5b1de7ff3150981
Scott Mon <Scott Mon>
2016-05-28 18:18:36 +0800

    more decent way to show the time

--- pycon_cfg.py
+++ pycon_cfg.py
@@ -93,7 +93,7 @@ logging.basicConfig(level=logging.DEBUG,
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+formatter = logging.Formatter('[%(asctime)s.%(msecs)03d] %(name)-12s: %(levelname)-8s %(message)s')
 console.setFormatter(formatter)
 
 logging.getLogger('').addHandler(console)
